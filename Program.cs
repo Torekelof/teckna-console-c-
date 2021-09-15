c31f4df3fd891ca74f734c6b9d6efa3f2a1b52ea
torekelof <ekelof.tor@elev.ga.ntig.se>
2021-09-15 10:05:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace teckna_console_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// Ändra bakgrundsfärg till mörkröd            
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+
+
+            // Ändra textfärgen till vit
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            Console.WriteLine("Ett testmeddelande");   
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.WriteLine("An error has ocurred.");
+            Console.WriteLine("Error: 0E : 016C : BFF415FA");
+             
+             
+             Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+         }
+    }
+}
